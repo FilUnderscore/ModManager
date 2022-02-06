36a764bf18cefd7505ae9e30dcbafc9512cf8e04
Filip Jerkovic <filjerkovic@gmail.com>
2022-02-06 23:10:22 +1300

    Redo modloader

--- Source/ModEntry.cs
+++ Source/ModEntry.cs
@@ -8,85 +8,13 @@ namespace CustomModManager
 {
     public class ModEntry
     {
-        internal readonly string folderName;
+        public readonly ModLoadInfo loadInfo;
         public readonly Mod instance;
-        public readonly ModInfo.ModInfo info;
 
-        private readonly EModDisableState modDisableState;
-        private readonly bool isLoaded = false;
-        private bool? willBeEnabled = null;
-
-        public readonly ModManifest manifest;
-
-        public ModEntry(string folderName, ModInfo.ModInfo info, ModManifest manifest)
+        public ModEntry(ModLoadInfo loadInfo, Mod modInstance)
         {
-            this.folderName = folderName;
-            this.info = info;
-            this.manifest = manifest;
-        }
-
-        public ModEntry(string folderName, Mod mod, EModDisableState modDisableState, ModManifest manifest) : this(folderName, mod.ModInfo, manifest)
-        {
-            this.instance = mod;
-
-            this.isLoaded = true;
-
-            this.modDisableState = modDisableState;
-        }
-
-        public bool IsLoaded()
-        {
-            return this.isLoaded;
-        }
-
-        public bool? WillBeEnabled()
-        {
-            return this.willBeEnabled;
-        }
-
-        public void Toggle()
-        {
-            if (this.willBeEnabled == null)
-                this.willBeEnabled = !this.isLoaded;
-            else
-                this.willBeEnabled = !this.willBeEnabled;
-        }
-
-        public bool HasBeenChanged()
-        {
-            return this.willBeEnabled != null && this.willBeEnabled.Value != this.isLoaded;
-        }
-
-        public EModDisableState GetModDisableState()
-        {
-            return this.modDisableState;
-        }
-
-        public enum EModDisableState
-        {
-            Allowed,
-            Disallowed,
-            Disallowed_Preloaded
-        }
-
-        public string GetModDisableStateReason()
-        {
-            switch (this.modDisableState)
-            {
-                case EModDisableState.Allowed:
-                    return "";
-                case EModDisableState.Disallowed:
-                    return Localization.Get("xuiModDisableStateReasonDisallowed");
-                case EModDisableState.Disallowed_Preloaded:
-                    return Localization.Get("xuiModDisableStateReasonDisallowedPreloaded");
-                default:
-                    return "";
-            }
-        }
-
-        public string GetVersion()
-        {
-            return manifest != null && manifest.Version != null ? manifest.Version.ToString() : info.Version.Value;
+            this.loadInfo = loadInfo;
+            this.instance = modInstance;
         }
     }
 }
---
+++ Source/ModLoadInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomModManager
+{
+    public class ModLoadInfo
+    {
+        public ModInfo.ModInfo modInfo;
+        public ModManifest manifest;
+        public int loadIndex;
+        public List<ModLoadDependency> dependencies;
+
+        public EModLoadState loadState;
+
+        public enum EModLoadState
+        {
+            Loaded,
+            Unloaded,
+            Error,
+            Missing_Dependencies
+        }
+
+        public class ModLoadDependency
+        {
+            public ModLoadInfo parent;
+            public ModLoadInfo child;
+        }
+    }
+}
