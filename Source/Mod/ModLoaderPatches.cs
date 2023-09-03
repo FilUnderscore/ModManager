71f159bb8ab3ee35a45b38649caec27ead371afe
FilUnderscore <fil@filunderscore.com>
2023-09-04 00:03:10 +1200

    Fix mods not loading properly

--- Source/Mod/ModLoaderPatches.cs
+++ Source/Mod/ModLoaderPatches.cs
@@ -11,9 +11,7 @@ namespace CustomModManager.Mod
         [HarmonyPatch(nameof(global::Mod.InitModCode))]
         private sealed class Mod_InitModCode_Patch
         {
-            private static bool INIT_MOD_CODE = false;
-
-            private static int PRE_INIT_COUNT = 0;
+            private static int PRE_INIT_COUNT = 1;
 
             private static bool Prefix()
             {
@@ -33,18 +31,18 @@ namespace CustomModManager.Mod
 
             private static void Postfix()
             {
-                if (INIT_MOD_CODE)
+                if (CAN_INIT_MOD_CODE)
                     return;
 
                 int modCount = MOD_COUNT;
                 CAN_INIT_MOD_CODE = PRE_INIT_COUNT == modCount;
-
+                
                 if(CAN_INIT_MOD_CODE)
                 {
-                    INIT_MOD_CODE = true;
-
                     foreach(var mod in ModLoader.Instance.GetMods(false))
                     {
+                        Log.Out("Mod " + mod.Info.Name + " enabled " + ModLoader.Instance.IsModEnabled(mod));
+
                         if(ModLoader.Instance.IsModEnabled(mod))
                             mod.Load();
                     }
